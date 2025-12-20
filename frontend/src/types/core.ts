2864d7697ca8a31572954c4e6359f9df32ae0198
Rahat <rajatseth9mm@gmail.com>
2025-12-20 20:56:27 +0530

    Update core.ts

--- frontend/src/types/core.ts
+++ frontend/src/types/core.ts
@@ -1,1 +1,15 @@
+export type AppMode = "CHART" | "RESULTS";
 
+export interface Candle {
+  time: number;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+}
+
+export interface PatternMatch {
+  startTime: number;
+  endTime: number;
+  similarity: number;
+}
